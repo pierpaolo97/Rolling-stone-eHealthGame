6c1afb1b0ad097f0200c841737b69750379f7621
lucadipalma <luca.dipalma@mail.polimi.it>
2021-12-20 12:52:09 +0100

    ultimaultimaultimaultima modifica

--- e-HealthGame/Assets/Script/diplaypause.cs
+++ e-HealthGame/Assets/Script/diplaypause.cs
@@ -6,10 +6,26 @@ using UnityEngine.UI;
 public class diplaypause : MonoBehaviour
 {
     public Text menuPauseText;
+    string word = "";
 
     private void Start()
     {
-        menuPauseText.text = "Stai giocando la lettera " + PlayerPrefs.GetString("LetteraLivello","C") +"\n" + " in modalità " + PlayerPrefs.GetString("difficolta","Easy");
+        string diff = PlayerPrefs.GetString("difficolta", "Easy");
+        switch (diff)
+        {
+            case "Easy":
+                word = "Facile";
+                break;
+
+            case "Medium":
+                word = "Media";
+                break;
+
+            case "Hard":
+                word = "Difficile";
+                break;
+        }
+        menuPauseText.text = "Stai giocando la lettera " + PlayerPrefs.GetString("LetteraLivello","C") +"\n" + " con difficoltà " + word;
     }
 
 }
